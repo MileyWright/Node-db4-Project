c296dfbeaf897c9b3b8388b023bd8940b7c5cb86
Miley Wright <dishaliekwright@yahoo.com>
2020-02-06 13:23:45 -0500

    added Recipes seed

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Recipes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('Recipes').insert([
+        {id: 1, name: 'Strawberry ShortCake', instructions: 'Dont forget the strawberrys.'},
+        {id: 2, name: 'Pizza', instructions: 'Whatever you do, dont add pineapples on the pizza'},
+        {id: 3, name: 'Cupcakes', instructions: 'Try not to put too much batter in the pan before baking'}
       ]);
     });
 };
