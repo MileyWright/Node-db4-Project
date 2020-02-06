7789113582307d8a7f32ec3670f4098b3647320a
Miley Wright <dishaliekwright@yahoo.com>
2020-02-06 00:52:18 -0500

    created recipes and ingredients migration tables

--- data/migrations/20200205175043_recipes.js
+++ data/migrations/20200205175043_recipes.js
@@ -1,6 +1,21 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('Recipes', tbl => {
+        tbl.increments()
+        tbl.string('name', 255)
+            .notNullable()
+        tbl.string('instructions', 255)
+            .notNullable();
+    })
+    .createTable('Ingredients', tbl => {
+        tbl.increments()
+        tbl.string('ingredient', 255)
+            .notNullable();
+    })
+    .createTable('Recipe_Book', tbl => {
+        
+    })
 };
 
 exports.down = function(knex) {
